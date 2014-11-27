a127ccd8c2cd9faf4a4a9ff0620eb1a56a93f2a3
lugang <lugang@lugangs-Mac-mini.local>
2014-11-27 10:20:12 +0800

    Initial Commit

---
+++ SDLAaudioPlayer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDLAaudioPlayer
+//
+//  Created by lugang on 11/27/14.
+//  Copyright (c) 2014 live365. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
